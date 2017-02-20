9ba6b5e2c3eb92c6b57128fd8bae0ad706ddf60d
João Conde <joaoconde_13@hotmail.com>
2017-02-20 18:26:20 +0000

    2nd map done: TODO: change hero-ogre interaction and ogre behaviour

--- DungeonKeep/src/Maze.java
+++ DungeonKeep/src/Maze.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
-public class Maze { 
+public class Maze {
 
 	private char[][] map1 = new char[10][10];
-	private char[][] map2 = new char[10][10];
+	private char[][] map2 = new char[9][9];
 
 	int invalid = 1000;
 	private int[] hero = { invalid, invalid }, guard = { invalid, invalid }, lever = { invalid, invalid },
 			ogre = { invalid, invalid };
-	
-	private char[] guardMoves = {'A','S','S','S','S','A','A','A','A','A','A','S','D','D','D','D','D','D','D','W','W','W','W','W'};
-	private int currentGuardMove = 0;	
-	
-	private char[][][] mapList = { map1, map2 }; 
+
+	private char[] guardMoves = { 'A', 'S', 'S', 'S', 'S', 'A', 'A', 'A', 'A', 'A', 'A', 'S', 'D', 'D', 'D', 'D', 'D',
+			'D', 'D', 'W', 'W', 'W', 'W', 'W' };
+	private int currentGuardMove = 0;
+
+	private char[][][] mapList = { map1, map2 };
 	private int currentMapIndex = 0;
 
 	public void run() {
@@ -21,11 +22,15 @@ public class Maze {
 
 		buildMap(1);
 
-		while (!end) { 
-			play();
+		while (!end) {
+			if (play()) {
+				currentMapIndex++;
+				buildMap(currentMapIndex + 1);
+			}
+
 			end = isGameOver();
 		}
-		
+
 	}
 
 	public void buildWalls(char[][] board) {
@@ -35,9 +40,8 @@ public class Maze {
 			board[0][i] = 'X';
 		}
 
-		
-		for (int i = 0; i < board[9].length; i++) {
-			board[9][i] = 'X';
+		for (int i = 0; i < board[board.length - 1].length; i++) {
+			board[board.length - 1][i] = 'X';
 		}
 
 		// filling right and left collumn
@@ -46,7 +50,7 @@ public class Maze {
 		}
 
 		for (int i = 0; i < board.length; i++) {
-			board[i][9] = 'X';
+			board[i][board.length - 1] = 'X';
 		}
 
 	}
@@ -90,24 +94,41 @@ public class Maze {
 			lever[0] = lin;
 			lever[1] = col;
 			break;
+
+		case 'O':
+			if (ogre[0] != invalid || ogre[1] != invalid) {
+				changeCell(ogre[0], ogre[1], ' ', board);
+			}
+			changeCell(lin, col, 'O', board);
+			ogre[0] = lin;
+			ogre[1] = col;
+			break;
 		}
 
 	}
 
-	public void openDoors(){
-	
-		switch(currentMapIndex){
-		
+	public void openDoors() {
+
+		switch (currentMapIndex) {
+
 		case 0:
-			changeCell(5,0,'S',mapList[currentMapIndex]);
-			changeCell(6,0,'S',mapList[currentMapIndex]);
+			changeCell(5, 0, 'S', mapList[currentMapIndex]);
+			changeCell(6, 0, 'S', mapList[currentMapIndex]);
 			break;
-		
+
+		case 1:
+			changeCell(1, 0, 'S', mapList[currentMapIndex]);
+
 		}
-		
+
 	}
 
 	public void buildMap(int mapNumber) {
+
+		hero[0] = hero[1] = invalid;
+		guard[0] = guard[1] = invalid;
+		ogre[0] = ogre[1] = invalid;
+		lever[0] = lever[1] = invalid;
 
 		switch (mapNumber) {
 
@@ -158,6 +179,21 @@ public class Maze {
 			break;
 
 		case 2:
+
+			// Building external walls for map2
+			buildWalls(map2);
+
+			// Placing doors in map2
+			changeCell(1, 0, 'I', map2);
+
+			// placing lever
+			moveCharacter(1, 7, 'k', map2);
+
+			// set Hero initial location
+			moveCharacter(7, 1, 'H', map2);
+
+			// set ogre initial location
+			moveCharacter(1, 4, 'O', map2);
 			break;
 		}
 	}
@@ -176,21 +212,21 @@ public class Maze {
 	public boolean isGameOver() {
 
 		// check if guard is in the 3 above positions of the hero
-		if (hero[0] - 1 == guard[0] && (hero[1] - 1 == guard[1] || hero[1] == guard[1] || hero[1] + 1 == guard[1])){
+		if (hero[0] - 1 == guard[0] && (hero[1] - 1 == guard[1] || hero[1] == guard[1] || hero[1] + 1 == guard[1])) {
 			printMaze(mapList[currentMapIndex]);
 			System.out.println("GAME OVER! Better luck next time!");
 			return true;
 		}
 
 		// check if guard is in the 3 below position of the hero
-		if (hero[0] + 1 == guard[0] && (hero[1] - 1 == guard[1] || hero[1] == guard[1] || hero[1] + 1 == guard[1])){
+		if (hero[0] + 1 == guard[0] && (hero[1] - 1 == guard[1] || hero[1] == guard[1] || hero[1] + 1 == guard[1])) {
 			printMaze(mapList[currentMapIndex]);
 			System.out.println("GAME OVER! Better luck next time!");
 			return true;
 		}
 
 		// check if guard is in the 3 remaining positions
-		if (hero[0] == guard[0] && (hero[1] - 1 == guard[1] || hero[1] + 1 == guard[1])){
+		if (hero[0] == guard[0] && (hero[1] - 1 == guard[1] || hero[1] + 1 == guard[1])) {
 			printMaze(mapList[currentMapIndex]);
 			System.out.println("/nGAME OVER!/nBetter luck next time!");
 			return true;
@@ -207,12 +243,7 @@ public class Maze {
 		return false;
 	}
 
-	public void play() {
-
-		boolean leverHidden = isLeverOverlapse();
-
-		printMaze(mapList[currentMapIndex]);
-
+	public char readUserInput() {
 		// reading user move
 		System.out.println("Enter your move:");
 		Scanner scan = new Scanner(System.in);
@@ -220,61 +251,83 @@ public class Maze {
 		char move = scan.next().charAt(0);
 		move = Character.toUpperCase(move);
 
+		return move;
+	}
+
+	public boolean play() {
+
+		boolean leverHidden = isLeverOverlapse();
+		boolean advancelvl = false;
+
+		printMaze(mapList[currentMapIndex]);
+
+		char move = readUserInput();
+
 		switch (move) {
 
 		case 'W':
+			if (mapList[currentMapIndex][hero[0] - 1][hero[1]] == 'S')
+				advancelvl = true;
 			moveCharacter(hero[0] - 1, hero[1], 'H', mapList[currentMapIndex]);
 			break;
 
 		case 'A':
+			if (mapList[currentMapIndex][hero[0]][hero[1] - 1] == 'S')
+				advancelvl = true;
 			moveCharacter(hero[0], hero[1] - 1, 'H', mapList[currentMapIndex]);
 			break;
 
 		case 'S':
+			if (mapList[currentMapIndex][hero[0] + 1][hero[1]] == 'S')
+				advancelvl = true;
 			moveCharacter(hero[0] + 1, hero[1], 'H', mapList[currentMapIndex]);
 			break;
 
 		case 'D':
+			if (mapList[currentMapIndex][hero[0]][hero[1] + 1] == 'S')
+				advancelvl = true;
 			moveCharacter(hero[0], hero[1] + 1, 'H', mapList[currentMapIndex]);
 			break;
 
 		}
-		
-		//move guard OBS.: he moves regardless of hero "moving", e.g if hero hits a wall, guard moves
-		
-		
-		switch (guardMoves[currentGuardMove]) {
 
-		case 'W':
-			moveCharacter(guard[0] - 1, guard[1], 'G', mapList[currentMapIndex]);
-			break;
+		// move guard OBS.: he moves regardless of hero "moving", e.g if hero
+		// hits a wall, guard moves
 
-		case 'A':
-			moveCharacter(guard[0], guard[1] - 1, 'G', mapList[currentMapIndex]);
-			break;
+		if (currentMapIndex == 0) {
+			switch (guardMoves[currentGuardMove]) {
 
-		case 'S':
-			moveCharacter(guard[0] + 1, guard[1], 'G', mapList[currentMapIndex]);
-			break;
+			case 'W':
+				moveCharacter(guard[0] - 1, guard[1], 'G', mapList[currentMapIndex]);
+				break;
 
-		case 'D':
-			moveCharacter(guard[0], guard[1] + 1, 'G', mapList[currentMapIndex]);
-			break;
+			case 'A':
+				moveCharacter(guard[0], guard[1] - 1, 'G', mapList[currentMapIndex]);
+				break;
 
+			case 'S':
+				moveCharacter(guard[0] + 1, guard[1], 'G', mapList[currentMapIndex]);
+				break;
+
+			case 'D':
+				moveCharacter(guard[0], guard[1] + 1, 'G', mapList[currentMapIndex]);
+				break;
+
+			}
 		}
-		
-		if(currentGuardMove != guardMoves.length-1)
+
+		if (currentGuardMove != guardMoves.length - 1)
 			currentGuardMove++;
 		else
 			currentGuardMove = 0;
-		
-		
-		if(hero[0] == lever[0] && hero[1] == lever[1])
+
+		if (hero[0] == lever[0] && hero[1] == lever[1])
 			openDoors();
-			
-		
+
 		if (leverHidden)
 			moveCharacter(lever[0], lever[1], 'k', mapList[currentMapIndex]);
 
+		return advancelvl;
 	}
+
 }
